17995c6cc2b2e51bcae6154227ee2e9a79dc8f8b
laragaloso <laragaloso>
2021-12-16 21:12:24 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY >20) {
+            $('.navbar').addClass("sticky");
+        }else {
+            $('.navbar').removeClass("sticky");
+        }
+
+    });
+    //toggle menu/navbar script 
+    
+});
+
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
